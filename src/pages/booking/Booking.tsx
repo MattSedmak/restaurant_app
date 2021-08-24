4eff87aed16f348fa4affe1ab5744b3506c2f8ad
MattSedmak <sedmakmatt@gmail.com>
2021-08-24 14:34:01 +0200

    Setup of Seating and booking components - all team members

--- src/pages/booking/Booking.tsx
+++ src/pages/booking/Booking.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import Guests from '../../components/guests/Guests';
+import Seating from '../../components/seating/Seating';
 const Booking = () => {
+  const [seating, setSeating] = useState(0);
+
+  const seatingHandler = (seatTime: number) => {
+    setSeating(seatTime);
+  };
+  console.log(seating);
   return (
     <div>
       <h2>Make a booking</h2>
+      <Seating onSeatTime={seatingHandler} />
+      <Guests />
     </div>
   );
 };
