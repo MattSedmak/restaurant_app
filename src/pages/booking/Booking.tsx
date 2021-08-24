eb44c63f2acc8ae2eef6ac2da4db98e64088120c
MattSedmak <sedmakmatt@gmail.com>
2021-08-24 11:43:31 +0200

    Initial commit - all team members

---
+++ src/pages/booking/Booking.tsx
@@ -0,0 +1,9 @@
+const Booking = () => {
+  return (
+    <div>
+      <h2>Make a booking</h2>
+    </div>
+  );
+};
+
+export default Booking;
---
+++ src/pages/start/Start.tsx
@@ -0,0 +1,7 @@
+export const Start = () => {
+  return (
+    <div>
+      <h1>Hej! </h1>
+    </div>
+  );
+};
