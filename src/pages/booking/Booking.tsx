2831b215bf058f42599f19897e1d7a1dac8ae9df
MattSedmak <sedmakmatt@gmail.com>
2021-08-26 16:05:39 +0200

    added axios for availability, connected till heroku server -- all team members

--- src/pages/booking/Booking.tsx
+++ src/pages/booking/Booking.tsx
@@ -1,9 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Guests from '../../components/guests/Guests';
 import Seating from '../../components/seating/Seating';
+import axios, { AxiosResponse } from 'axios';
+import { IAvailable } from '../../models/IAvailable';
 const Booking = () => {
   const [seating, setSeating] = useState(0);
   const [guests, setGuests] = useState(0);
+
+  const baseUrl: string = 'https://thedudes-restaurant.herokuapp.com';
+  // const baseUrl: string = 'http://localhost:4000';
+
+  const getAvailability = async () => {
+    try {
+      const res = await axios.get<IAvailable>(baseUrl + '/availability', {
+        params: {
+          guests: guests,
+          seating: seating,
+        },
+      });
+      console.log(res);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAvailability();
+  }, [guests, seating]);
 
   const seatingHandler = (seatTime: number) => {
     setSeating(seatTime);
@@ -12,7 +35,7 @@ const Booking = () => {
   const guestHandler = (guestNumber: number) => {
     setGuests(guestNumber);
   };
-  console.log(guests);
+
   return (
     <div>
       <h2>Make a booking</h2>
