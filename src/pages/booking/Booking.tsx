b8230d9f79fd5b9ca048d52b84bd6f9c211365bf
johced <johan.cederstrand@bahnhof.se>
2021-08-27 09:55:55 +0200

    added react-calendar - npm i req again - all team members

--- src/pages/booking/Booking.tsx
+++ src/pages/booking/Booking.tsx
@@ -3,9 +3,14 @@ import Guests from '../../components/guests/Guests';
 import Seating from '../../components/seating/Seating';
 import axios, { AxiosResponse } from 'axios';
 import { IAvailable } from '../../models/IAvailable';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 const Booking = () => {
   const [seating, setSeating] = useState(0);
   const [guests, setGuests] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [isAvailable, setIsAvailable] = useState(true);
 
   const baseUrl: string = 'https://thedudes-restaurant.herokuapp.com';
   // const baseUrl: string = 'http://localhost:4000';
@@ -19,6 +24,7 @@ const Booking = () => {
         },
       });
       console.log(res);
+      console.log(res.data);
       return res;
     } catch (error) {
       console.log(error);
@@ -35,12 +41,15 @@ const Booking = () => {
   const guestHandler = (guestNumber: number) => {
     setGuests(guestNumber);
   };
-
+  console.log(date);
   return (
     <div>
       <h2>Make a booking</h2>
       <Seating onSeatTime={seatingHandler} />
       <Guests onGuestSelect={guestHandler} />
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} minDate={new Date()} />
+      </div>
     </div>
   );
 };
