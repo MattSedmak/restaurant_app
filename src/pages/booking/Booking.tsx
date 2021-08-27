61a6a29621abe2e32121ac8bfddee0fbb79b0d86
FredrikAndre <fredrik.he.andre@gmail.com>
2021-08-27 11:43:35 +0200

    Created states to get info to Calendar || all team members

--- src/pages/booking/Booking.tsx
+++ src/pages/booking/Booking.tsx
@@ -4,35 +4,44 @@ import Seating from '../../components/seating/Seating';
 import axios, { AxiosResponse } from 'axios';
 import { IAvailable } from '../../models/IAvailable';
 import Calendar from 'react-calendar';
+import { differenceInCalendarDays } from 'date-fns'
 import 'react-calendar/dist/Calendar.css';
 
 const Booking = () => {
   const [seating, setSeating] = useState(0);
   const [guests, setGuests] = useState(0);
   const [date, setDate] = useState(new Date());
-  const [isAvailable, setIsAvailable] = useState(true);
+  const [allBookings, setAllBookings] = useState<IAvailable[]>([]);
+  const [isNotAvailable, setIsNotAvailable] = useState<IAvailable[]>([]);
 
-  const baseUrl: string = 'https://thedudes-restaurant.herokuapp.com';
-  // const baseUrl: string = 'http://localhost:4000';
+
+  // const baseUrl: string = 'https://thedudes-restaurant.herokuapp.com';
+  const baseUrl: string = 'http://localhost:4000';
 
   const getAvailability = async () => {
     try {
-      const res = await axios.get<IAvailable>(baseUrl + '/availability', {
+      const res = await axios.get(baseUrl + '/availability', {
         params: {
           guests: guests,
           seating: seating,
         },
-      });
-      console.log(res);
-      console.log(res.data);
-      return res;
+      }); 
+      setAllBookings(res.data) 
     } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
     getAvailability();
+    filterDates();
   }, [guests, seating]);
+
+  const filterDates = () => {
+    const someDates = allBookings.filter
+    ((booking) => booking.isAvailable === false)
+    
+  }
 
   const seatingHandler = (seatTime: number) => {
     setSeating(seatTime);
@@ -41,14 +50,16 @@ const Booking = () => {
   const guestHandler = (guestNumber: number) => {
     setGuests(guestNumber);
   };
-  console.log(date);
   return (
     <div>
       <h2>Make a booking</h2>
       <Seating onSeatTime={seatingHandler} />
       <Guests onGuestSelect={guestHandler} />
       <div className='calendar-container'>
-        <Calendar onChange={setDate} value={date} minDate={new Date()} />
+        <Calendar 
+          onChange={setDate} 
+          value={date} 
+          minDate={new Date()} />
       </div>
     </div>
   );
