bf7e3a5d68916b552be93f98d3d30176db17955e
MattSedmak <sedmakmatt@gmail.com>
2021-09-09 15:09:12 +0200

    Added 404 page

---
+++ src/pages/Page404/PageNotFoundStyles.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+export const Container = styled.div`
+  width: 80%;
+  max-width: 40em;
+  margin: 5em auto;
+  padding: 2em;
+  /* background-color: #fff; */
+  text-align: center;
+
+  p {
+    font-size: 3em;
+    margin-bottom: 2em;
+  }
+`;
+
+export const Logo = styled.h1`
+  font-family: 'Kalam', cursive;
+  font-size: 3em;
+`;
+
+export const StyledLink = styled(Link)`
+  color: blue;
+  text-decoration: none;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
