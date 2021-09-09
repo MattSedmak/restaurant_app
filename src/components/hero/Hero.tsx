d8aff358fc5c52cf39f162739286a8b2220bae04
johced <johan.cederstrand@bahnhof.se>
2021-09-09 11:15:05 +0200

    Added a simple getByText test on Hero component, had to use MemoryRouter because of the Link on landingpage

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,28 +1,39 @@
-import { useState } from "react"
-import Navbar from "../navbar/Navbar"
-import Sidebar from "../sidebar/Sidebar"
-import { HeroContainer, HeroContent, HeroItems, HeroTitle, HeroText, NavLink } from "./HeroStyle"
+import { useState } from 'react';
+import Navbar from '../navbar/Navbar';
+import Sidebar from '../sidebar/Sidebar';
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroTitle,
+  HeroText,
+  NavigationLink,
+} from './HeroStyle';
 
 const Hero = () => {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleSideBar = () => {
-        setIsOpen(!isOpen)
-    }
+  const toggleSideBar = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <HeroContainer>
-            <Navbar toggle={toggleSideBar}/>
-            <Sidebar sideBar={isOpen} toggle={toggleSideBar}/>
-            <HeroContent>
-                <HeroItems>
-                    <HeroTitle>Eat<br/> Drink<br/> Dream</HeroTitle>
-                    <HeroText>En oförglömlig upplevelse</HeroText>
-                    <NavLink to="/booking">Boka bord</NavLink>
-                </HeroItems>
-            </HeroContent>
-        </HeroContainer>
-    )
-}
+  return (
+    <HeroContainer>
+      <Navbar toggle={toggleSideBar} />
+      <Sidebar sideBar={isOpen} toggle={toggleSideBar} />
+      <HeroContent>
+        <HeroItems>
+          <HeroTitle>
+            Eat
+            <br /> Drink
+            <br /> Dream
+          </HeroTitle>
+          <HeroText>En oförglömlig upplevelse</HeroText>
+          <NavigationLink to='/booking'>Boka bord</NavigationLink>
+        </HeroItems>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
 
-export default Hero
+export default Hero;
